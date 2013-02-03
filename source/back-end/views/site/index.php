1a1aa93f268688d38636978ec4eb9fde023e4800
lebaotriet <lebaotriet@gmail.com>
2013-02-03 11:04:46 +0700

    commited from lebaotriet

--- source/back-end/views/site/index.php
+++ source/back-end/views/site/index.php
@@ -7,7 +7,7 @@ $this->pageTitle=Yii::app()->name;
 ?>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?> Back-end side BBB</i></h1>
-
+<h3>This is a line from lebaotriet</h3>
 <p>Congratulations! You have successfully created your Yii application.</p>
 
 <p>You may change the content of this page by modifying the following two files:</p>
